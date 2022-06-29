79ada93674d832c06a661a4689090965dfd6236e
lachetti <iluja.mineev@gmail.com>
2022-06-29 12:19:52 +0300

    modified logger

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -4,29 +4,29 @@ import util from 'util';
 
 const appendFile = util.promisify(fs.appendFile);
 
-class Logger {
-  private _logFilePath = '/../../bot.log';
-  private _fontColors = {
-    black: '\x1b[30m',
-    red: '\x1b[31m',
-    green: '\x1b[32m',
-    yellow: '\x1b[33m',
-    blue: '\x1b[34m',
-    magenta: '\x1b[35m',
-    cyan: '\x1b[36m',
-    white: '\x1b[37m',
-    crimson: '\x1b[38m'
-  };
+const fontColors = {
+  black: '\x1b[30m',
+  red: '\x1b[31m',
+  green: '\x1b[32m',
+  yellow: '\x1b[33m',
+  blue: '\x1b[34m',
+  magenta: '\x1b[35m',
+  cyan: '\x1b[36m',
+  white: '\x1b[37m',
+  crimson: '\x1b[38m'
+};
 
-  constructor() {}
+class Logger {
+  private _logFilePath = '/../../.log';
 
   private _padTime(x: number) {
     return x.toString().padStart(2, '0');
   }
 
-  private _getPrefix(level: string) {
+  private _getNiceDateTime() {
     const date = new Date();
-    const time = '[' +
+
+    return '[' +
       this._padTime(date.getDay()) + '.' +
       this._padTime(date.getMonth() + 1) + '.' +
       this._padTime(date.getFullYear()) + ' ' +
@@ -35,44 +35,56 @@ class Logger {
       this._padTime(date.getSeconds()) + ':' +
       this._padTime(date.getMilliseconds()) +
     ']';
-
-    return `${time} ${level}: `;
   }
 
-  private _colorize(str: string, color: string) {
-    return color + str + this._fontColors.white;
+  private _colorize(str: string, color: keyof typeof fontColors) {
+    return fontColors[color] + str + fontColors.white;
   }
 
-  private _writeLog(log: string) {
-    appendFile(__dirname + this._logFilePath, log + '\n')
+  private _getPrefix(level: string) {
+    return `${this._getNiceDateTime()} ${level}: `;
+  }
+
+  private _writeLog(prefix: string, ...args: unknown[]) {
+    const log = args
+      .map(arg => typeof arg === 'object' ? JSON.stringify(arg) : arg)
+      .join(' ');
+
+    appendFile(__dirname + this._logFilePath, prefix + log + '\n')
       .catch(e => console.error(e.code, e.message));
   }
 
   log(...args: unknown[]) {
     const prefix = this._getPrefix('LOG');
 
-    console.log(this._colorize(prefix, this._fontColors.green), ...args);
+    console.log(this._colorize(prefix, 'green'), ...args);
   }
 
   info(...args: unknown[]) {
     const prefix = this._getPrefix('INFO');
 
-    console.log(this._colorize(prefix, this._fontColors.cyan), ...args);
-    this._writeLog(prefix + args.join(' '));
+    console.log(this._colorize(prefix, 'cyan'), ...args);
+    this._writeLog(prefix, ...args);
   }
 
   warn(...args: unknown[]) {
     const prefix = this._getPrefix('WARN');
 
-    console.warn(this._colorize(prefix, this._fontColors.yellow), ...args);
-    this._writeLog(prefix + args.join(' '));
+    console.warn(this._colorize(prefix, 'yellow'), ...args);
+    this._writeLog(prefix, ...args);
   }
 
   error(...args: unknown[]) {
     const prefix = this._getPrefix('ERROR');
 
-    console.error(this._colorize(prefix, this._fontColors.red), ...args);
-    this._writeLog(prefix + args.join(' '));
+    console.error(this._colorize(prefix, 'red'), ...args);
+    this._writeLog(prefix, ...args);
+  }
+
+  write(...args: unknown[]) {
+    const prefix = this._getPrefix('WRITE');
+
+    this._writeLog(prefix, ...args);
   }
 }
 
